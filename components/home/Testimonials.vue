d8addfb7364913596955005ae96deffd3ddf0776
saket <aryansaket@yahoo.com>
2022-04-12 21:02:55 +0530

    Add minor features

--- components/home/Testimonials.vue
+++ components/home/Testimonials.vue
@@ -38,6 +38,15 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+/* Hide scrollbar for Chrome, Safari and Opera */
+.no-scrollbar::-webkit-scrollbar {
+    display: none;
+}
 
+/* Hide scrollbar for IE, Edge and Firefox */
+.no-scrollbar {
+    -ms-overflow-style: none;  /* IE and Edge */
+    scrollbar-width: none;  /* Firefox */
+}
 </style>
